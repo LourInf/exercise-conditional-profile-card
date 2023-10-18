a21cdb4cbb1241106ee0af2acebcdf6ff69f0160
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-10-18 19:13:51 +0000

    JS modified

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ import "../style/index.css";
  *  This function is called every time the user changes types or changes any input
  * 
     {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
+        includeCover: true, // if includeCover is true the algorithm should show the cover image. False to not include
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
         socialMediaPosition: "right", // social media bar position (left or right)
@@ -22,25 +22,44 @@ import "../style/index.css";
         city: null
     }
  */
+
+//render function that receives variables and contains the logic to create most of the website HTML code
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  // Dynamically set the city and country based on variables
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : ""}<span> ${
+    variables.lastName ? variables.lastName : ""
+  }</span></h1>
+          <h2>${variables.role ? variables.role : ""}</h2>
+          <h3>${variables.city ? variables.city : ""}, ${
+    variables.country ? variables.country : ""
+  }</h3>
+          <ul class="${
+            variables.socialMediaPosition ? variables.socialMediaPosition : ""
+          }">
+            <li><a href="https://twitter.com/${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
